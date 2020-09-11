2a2d9b0e0aece120f34f756b99eb67e5fa2514f3
Edgar Runnman <edgar.runnman@gmail.com>
2020-09-11 23:03:16 +0200

    Bob PASS

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -2,8 +2,46 @@ using System;
 
 public static class Bob
 {
+    private static bool _isQuestion;
+    private static bool _isYell;
+    private static bool _containsLowLevelChar;
+    private static bool _isEmpty;
+
+
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        statement = statement.Replace("\n", String.Empty);
+        statement = statement.Replace("\r", String.Empty);
+        statement = statement.Replace("\t", String.Empty);
+        statement = statement.Replace(" ", String.Empty);
+        _isQuestion = statement.EndsWith("?");
+        _isYell = false;
+        _containsLowLevelChar = false;
+        foreach (char character in statement)
+        {
+            if (char.GetUnicodeCategory(character) != char.GetUnicodeCategory('a') && char.GetUnicodeCategory(character) != char.GetUnicodeCategory('A'))
+            {
+                continue;
+            }
+            else if (char.GetUnicodeCategory(character) == char.GetUnicodeCategory('a'))
+            {
+                _containsLowLevelChar = true;
+                _isYell = false;
+            }
+            else if (char.GetUnicodeCategory(character) == char.GetUnicodeCategory('A'))
+            {
+                _isYell = true;
+            }
+        }
+        _isEmpty = (statement.Length == 0) ? true : false;
+
+        if (_isQuestion && _isYell && !_containsLowLevelChar) return "Calm down, I know what I'm doing!";
+        else if (_isQuestion && _containsLowLevelChar) return "Sure.";
+        else if (_isQuestion) return "Sure.";
+        else if (_isYell && !_containsLowLevelChar) return "Whoa, chill out!";
+        else if (_isEmpty) return "Fine. Be that way!";
+        else return "Whatever.";
     }
+
+
 }
