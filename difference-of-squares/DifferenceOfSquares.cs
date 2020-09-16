50718879e69cac1436308198b0f354b479832743
Edgar Runnman <edgar.runnman@gmail.com>
2020-09-16 15:51:46 +0200

    DifferenceOfSquares PASS

--- difference-of-squares/DifferenceOfSquares.cs
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -4,16 +4,23 @@ public static class DifferenceOfSquares
 {
     public static int CalculateSquareOfSum(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string maxString = max.ToString();
+        int sum = 0;
+        for (int i = 1; i <= max; i++) sum += i;
+        return (int)Math.Pow((double)sum, 2.0);
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string maxString = max.ToString();
+        int sum = 0;
+        for (int i = 1; i <= max; i++) sum += (int)Math.Pow((double)i, 2.0);
+        return sum;
     }
 
     public static int CalculateDifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
     }
+
 }
