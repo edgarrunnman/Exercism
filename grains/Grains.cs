479dd3fd535fcbbb650a8a559dabfad7a88e78ac
Edgar Runnman <edgar.runnman@gmail.com>
2020-09-16 19:58:45 +0200

    Grains PASS

---
+++ grains/Grains.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n <= 0 || n > 64) throw new ArgumentOutOfRangeException();
+        ulong amountOfGrainsOnCell = 1;
+        int i = 1;
+        while (n > i++) amountOfGrainsOnCell *= 2;
+        return amountOfGrainsOnCell;
+    }
+    public static ulong Total()
+    {
+        ulong amountOfGrainsOnCell = 1;
+        ulong amountOfGrainsTotall = 1;
+        int i = 1;
+        while (64 > i++)
+        {
+            amountOfGrainsOnCell *= 2;
+            amountOfGrainsTotall += amountOfGrainsOnCell;
+        }
+        return amountOfGrainsTotall;
+    }
+}
