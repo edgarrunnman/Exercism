feb830391c8ea175b705854dfb9fd65b6ae03fe3
Edgar Runnman <edgar.runnman@gmail.com>
2020-09-16 20:42:52 +0200

    Pangram PASS

---
+++ pangram/Pangram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Pangram
+{    
+    public static bool IsPangram(string input)
+    {
+        List<char> characterCollection = new List<char>();
+        foreach (var value in Enumerable.Range((int)'a', ((int)'z' - (int)'a')))
+            characterCollection.Add(Convert.ToChar(value));
+        foreach (var character in input.ToLower()) 
+            try { characterCollection.Remove(character); } catch { }
+        if (characterCollection.Count == 0) 
+            return true;
+        else 
+            return false;
+    }
+}
