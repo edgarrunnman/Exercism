64afc11bebf667cf3ada59a644dd1c7ee780164e
Edgar Runnman <edgar.runnman@gmail.com>
2020-10-12 21:01:58 +0200

    progress

---
+++ acronym/Acronym.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Acronym
+{
+    public static class Acronym
+    {
+        public static string Abbreviate(string phrase)
+        {
+            var chars = phrase.ToCharArray().ToList();
+            chars.Remove('_');
+            var index = chars.FindIndex(i => i == '-');
+            if (index != -1 )
+                chars[index] = ' ';
+            return string
+                .Join("",string
+                    .Join("", chars)
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(c => c[0]))
+                .ToUpper();
+        }
+    }
+}
---
+++ bank-account/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class BankAccount
+{
+    private bool _aktivStatus { get; set; } = false;
+    private decimal _balance = 0;
+    public void Open()
+    {
+        this._aktivStatus = true;
+    }
+
+    public void Close()
+    {
+        this._aktivStatus = false;
+    }
+
+    public decimal Balance
+    {
+        get 
+        {
+            if (_aktivStatus)
+            return _balance;
+            throw new InvalidOperationException();
+        }
+    }
+
+    public void UpdateBalance(decimal change)
+    {
+        _balance = _balance + change;
+    }
+}
